8793f0caf30e76809852e191acf7d5cfe960f27c
Tô Thành Công <tcvn1985@gmail.com>
2015-01-24 22:04:05 +0700

    fix loi thua chu a

--- graylog2/graylog2-client-v2.sh
+++ graylog2/graylog2-client-v2.sh
@@ -11,8 +11,8 @@ cp /etc/rsyslog.d/50-default.conf /etc/rsyslog.d/50-default.conf.bka
 
 #sed -i -e 's|*.*;auth,authpriv.none|#*.*;auth,authpriv.none|' /etc/rsyslog.d/50-default.conf
 echo '$template GRAYLOG2-1,"<%PRI%>1 %timegenerated:::date-rfc3339% %hostname% %syslogtag% - %APP-NAME%: %msg:::drop-last-lf%\n"' >> /etc/rsyslog.d/50-default.conf
-echo '$template GRAYLOG2-2,"<%pri%>1 %timegenerated:::date-rfc3339% %fromhost% %app-name% %procid% %msg%\n"'  >> -a /etc/rsyslog.d/50-default.conf
-echo '$template GRAYLOGRFC5424,"<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msg%\n"' >> -a /etc/rsyslog.d/50-default.conf
+echo '$template GRAYLOG2-2,"<%pri%>1 %timegenerated:::date-rfc3339% %fromhost% %app-name% %procid% %msg%\n"'  >> /etc/rsyslog.d/50-default.conf
+echo '$template GRAYLOGRFC5424,"<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msg%\n"' >>  /etc/rsyslog.d/50-default.conf
 echo '$PreserveFQDN on' >> /etc/rsyslog.d/50-default.conf
 echo "*.* @$IP_GRAYLOG2:514;GRAYLOG2-2" >>  /etc/rsyslog.d/50-default.conf
 
