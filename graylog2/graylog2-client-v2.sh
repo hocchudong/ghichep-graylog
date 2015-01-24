f4e1d4ac158f21f063b98062ff943e8a4513f0dd
Tô Thành Công <tcvn1985@gmail.com>
2015-01-24 21:58:46 +0700

    fix loi ghi de
    fix loi ghi de

--- graylog2/graylog2-client-v2.sh
+++ graylog2/graylog2-client-v2.sh
@@ -10,11 +10,11 @@ apt-get -qq update
 cp /etc/rsyslog.d/50-default.conf /etc/rsyslog.d/50-default.conf.bka
 
 #sed -i -e 's|*.*;auth,authpriv.none|#*.*;auth,authpriv.none|' /etc/rsyslog.d/50-default.conf
-echo '$template GRAYLOG2-1,"<%PRI%>1 %timegenerated:::date-rfc3339% %hostname% %syslogtag% - %APP-NAME%: %msg:::drop-last-lf%\n"' | tee /etc/rsyslog.d/50-default.conf
-echo '$template GRAYLOG2-2,"<%pri%>1 %timegenerated:::date-rfc3339% %fromhost% %app-name% %procid% %msg%\n"'  | tee -a /etc/rsyslog.d/50-default.conf
-echo '$template GRAYLOGRFC5424,"<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msg%\n"' | tee -a /etc/rsyslog.d/50-default.conf
-echo '$PreserveFQDN on' | tee -a  /etc/rsyslog.d/50-default.conf
-echo "*.* @$IP_GRAYLOG2:514;GRAYLOG2-2" | tee -a  /etc/rsyslog.d/50-default.conf
+echo '$template GRAYLOG2-1,"<%PRI%>1 %timegenerated:::date-rfc3339% %hostname% %syslogtag% - %APP-NAME%: %msg:::drop-last-lf%\n"' >> /etc/rsyslog.d/50-default.conf
+echo '$template GRAYLOG2-2,"<%pri%>1 %timegenerated:::date-rfc3339% %fromhost% %app-name% %procid% %msg%\n"'  >> -a /etc/rsyslog.d/50-default.conf
+echo '$template GRAYLOGRFC5424,"<%pri%>%protocol-version% %timestamp:::date-rfc3339% %HOSTNAME% %app-name% %procid% %msg%\n"' >> -a /etc/rsyslog.d/50-default.conf
+echo '$PreserveFQDN on' >> /etc/rsyslog.d/50-default.conf
+echo "*.* @$IP_GRAYLOG2:514;GRAYLOG2-2" >>  /etc/rsyslog.d/50-default.conf
 
 echo "Restarting rsyslog"
 service rsyslog restart
