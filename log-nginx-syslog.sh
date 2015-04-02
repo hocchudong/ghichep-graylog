fb63a7e5c7a5db5f9959e3e53373795d421509db
Tô Thành Công <congtt.forum@gmail.com>
2015-04-02 16:44:18 +0700

    cau hinh log cho nginx lam viec voi rsyslog

---
+++ log-nginx-syslog.sh
@@ -0,0 +1,34 @@
+﻿#!/bin/bash 
+# Cau hinh cho NGINX day log qua rsyslog
+
+########
+
+# Tao file /etc/rsyslog.d/nginx.conf
+
+cat << EOF > /etc/rsyslog.d/nginx.conf
+
+EOF# error log
+$InputFileName /var/log/nginx/error.log
+$InputFileTag nginx:
+$InputFileStateFile stat-nginx-error
+$InputFileSeverity error
+$InputFileFacility local6
+$InputFilePollInterval 1
+$InputRunFileMonitor
+
+# access log
+$InputFileName /var/log/nginx/access.log
+$InputFileTag nginx:
+$InputFileStateFile stat-nginx-access
+$InputFileSeverity notice
+$InputFileFacility local6
+$InputFilePollInterval 1
+$InputRunFileMonitor
+EOF
+
+# Sua file /etc/rsyslog.conf
+cp /etc/rsyslog.conf /etc/rsyslog.conf.orig
+
+# Them dong moi
+sed -e  "s/\$ModLoad imklog   # provides kernel logging support/\$ModLoad imklog   # provides kernel logging support\n\
+\$ModLoad imfile # module log for Nginx/g"   /etc/rsyslog.conf
