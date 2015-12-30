a248ac84ab022b3a03f11378b34f1ac31ecffd77
Tô Tướng Công <tcvn1985@gmail.com>
2015-12-30 16:57:46 +0700

    update gralog 1.3

--- graylog/graylog-server.sh
+++ graylog/graylog-server.sh
@@ -31,8 +31,8 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 
 # For Graylog
-wget https://packages.graylog2.org/repo/packages/graylog-1.0-repository-ubuntu14.04_latest.deb
-dpkg -i graylog-1.0-repository-ubuntu14.04_latest.deb
+wget https://packages.graylog2.org/repo/packages/graylog-1.3-repository-ubuntu14.04_latest.deb
+dpkg -i graylog-1.3-repository-ubuntu14.04_latest.deb
 apt-get install apt-transport-https
 
 # Update
